fb2cfd2b17aa0b92ba407bc7eef2584f01b9e59b
Hai Rui Zhang <zhhairui@cn.ibm.com>
2014-08-27 19:35:59 +0800

    enable http_code

--- rest/app/api/router_manager.py
+++ rest/app/api/router_manager.py
@@ -1,7 +1,7 @@
 from lib.bottle import request, route, response
-from app.utils.logger import DCLogger
-from app.utils.config import Config
-from app.controller.router_controller import RouterController
+from utils.logger import DCLogger
+from utils.config import Config
+from controller.router import RouterController
 import json
 
 config = Config.getConfig()
@@ -17,6 +17,7 @@ router_controller = RouterController(config,logger)
 @route('/router/hosts', method = ['GET'])
 def get_hosts():
 	status, content = router_controller.get_hosts()
+	set_http_status(status, response)
 	result = {"status":status,"content":content}
 	return json.dumps(result)
 
@@ -25,11 +26,19 @@ def create_host():
 	body = request.json
 	name = body['host']
 	status, content = router_controller.create_host(name)
+	set_http_status(status, response)
 	result = {"status":status,"content":content}
 	return json.dumps(result)
 
 @route('/router/hosts/:host', method = ['DELETE'])
 def delete_host(host):
 	status, content = router_controller.delete_host(host)
+	set_http_status(status, response)
 	result = {"status":status,"content":content}
 	return json.dumps(result)
+
+def set_http_status(status, resp):
+	if status:
+		resp.status = 200
+	else:
+		resp.status = 400
