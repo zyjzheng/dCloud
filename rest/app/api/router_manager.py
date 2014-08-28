25010d0ff72e753669cceb16725d267e89ce7add
Hai Rui Zhang <zhhairui@cn.ibm.com>
2014-08-28 09:58:25 +0800

    reset error

--- rest/app/api/router_manager.py
+++ rest/app/api/router_manager.py
@@ -17,6 +17,7 @@ router_controller = RouterController(config,logger)
 @route('/router/hosts', method = ['GET'])
 def get_hosts():
 	status, content = router_controller.get_hosts()
+	set_http_status(status, response)
 	result = {"status":status,"content":content}
 	return json.dumps(result)
 
@@ -25,11 +26,19 @@ def create_host():
 	body = request.json
 	name = body['host']
 	status, content = router_controller.create_host(name)
+	set_http_status(status, response)
 	result = {"status":status,"content":content}
 	return json.dumps(result)
 
 @route('/router/hosts/:host', method = ['DELETE'])
 def delete_host(host):
 	status, content = router_controller.delete_host(host)
+	set_http_status(status, response)
 	result = {"status":status,"content":content}
 	return json.dumps(result)
+
+def set_http_status(status, resp):
+	if status:
+		resp.status = 200
+	else:
+		resp.status = 400
